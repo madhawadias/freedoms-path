2e48421c76526eb32b0aba07a638f540533aa90d
madhawadias <madhawadias@gmail.com>
2020-06-25 18:18:28 +0530

    Added Authentication service

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from "firebase";
+// import { AuthenticationService } from  '../authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  public managerData: any = [];
+
+  //constructor(private  authService:  AuthenticationService) { }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getManagers(){
+    let self = this;
+    firebase.database().ref('/managers/').once('value').then(function(snapshot) {
+      self.managerData = snapshot.val();
+      // self.teamsData1 = snapshot.val();
+      console.log(snapshot.val())
+    });
+  }
+
+  createManager(){
+    firebase.database().ref('managers/0').set({
+      name: "Keheliya Amarasinghe",
+      username: "keheliyaa",
+      password: 7,
+      team: [1,2],
+
+    });
+  }
+
+}
