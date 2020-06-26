2991a9fbd8b298cb4a189061f851cc46696a5ccd
madhawadias <madhawadias@gmail.com>
2020-06-26 06:45:24 +0530

    login created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from "firebase";
+import {Router} from "@angular/router";
 import { AuthenticationService } from  '../authentication.service';
 
 @Component({
@@ -11,9 +12,33 @@ export class LoginComponent implements OnInit {
   public managerData: any = [];
 
   //constructor(private  authService:  AuthenticationService) { }
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // this.createManager()
+  }
+
+  login(username,password){
+    this.getManagers();
+    let routerOne = this.router;
+
+    // let routerOne = this.router
+    console.log(username,password)
+    firebase.database().ref('/managers/').orderByChild("username").equalTo(username).on('value',function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        console.log("Username : "+childSnapshot.val().username + " Pass : "+ childSnapshot.val().password )
+        if (childSnapshot.val().username==username && childSnapshot.val().password==password){
+          console.log("Login Successful")
+          routerOne.navigate(["manager-dashboard", {id:childSnapshot.key}])
+        }
+        if(childSnapshot.val().username==username && childSnapshot.val().password!=password){
+          console.log("Username Exists, Wrong password")
+        }
+        // else {
+        //   console.log("Invalid Username")
+        // }
+      })
+    });
 
   }
 
